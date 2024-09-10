495e39c951141b9a33ed2658bb0935a75dc9c81e
The PHP Team <139501085+ThePHPTeam@users.noreply.github.com>
2024-09-10 14:17:02 -0300

    Add files via upload

---
+++ WAWP/Pages/BroadcastsListsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WAWP.Assets.Pages
+{
+    public partial class BroadcastsListsPage : PhoneApplicationPage
+    {
+        public BroadcastsListsPage()
+        {
+            InitializeComponent();
+        }
+
+
+        //lol im so funy rn
+        private void yaymap(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("YAY MAP!", "WAWP Button Test", MessageBoxButton.OK);
+        }
+    }
+}
