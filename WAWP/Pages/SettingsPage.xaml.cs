54e4436715faee9ccf7830c3ad54cf0b1b6a22d0
Kierownik223 <marcel@klos.net.pl>
2024-09-12 21:45:54 +0200

    settingspage.xaml

--- WAWP/Pages/SettingsPage.xaml.cs
+++ WAWP/Pages/SettingsPage.xaml.cs
@@ -16,5 +16,20 @@ namespace WAWP.Assets.Pages
         {
             InitializeComponent();
         }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            List<SettingsItem> settings = new List<SettingsItem>();
+            settings.Add(new SettingsItem() { name = "about", desc = "meet the Team" });
+            settings.Add(new SettingsItem() { name = "contacts", desc = "invite a friend, block" });
+            settings.Add(new SettingsItem() { name = "account", desc = " privacy, delete account" });
+            settings.Add(new SettingsItem() { name = "profile", desc = "name, set an avatar, status" });
+            settings.Add(new SettingsItem() { name = "chat settings", desc = "auto-download, wallpaper, backup" });
+            settings.Add(new SettingsItem() { name = "lock screen", desc = "add WAWP on a lock screen" });
+            settings.Add(new SettingsItem() { name = "notification sounds", desc = "change notification sounds" });
+            settings.Add(new SettingsItem() { name = "rotation", desc = "auto-rotate or block rotation" });
+            settings.Add(new SettingsItem() { name = "connectivity", desc = "change connectivity type" });
+
+            SettingsList.ItemsSource = settings;
+        }
     }
 }
