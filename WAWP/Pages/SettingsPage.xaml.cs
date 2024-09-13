e86e519fce7ce640fd3adfb01579285da1ed834e
PickaxeCar <82699403+PickaxeCar@users.noreply.github.com>
2024-09-13 19:20:50 +0300

    Add files via upload
    kinda working settings navigation

--- WAWP/Pages/SettingsPage.xaml.cs
+++ WAWP/Pages/SettingsPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using WAWP.Resources;
 
 namespace WAWP.Assets.Pages
 {
@@ -19,17 +20,34 @@ namespace WAWP.Assets.Pages
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             List<SettingsItem> settings = new List<SettingsItem>();
-            settings.Add(new SettingsItem() { Name = "about", Description = "meet the Team" });
-            settings.Add(new SettingsItem() { Name = "contacts", Description = "invite a friend, block" });
-            settings.Add(new SettingsItem() { Name = "account", Description = "privacy, delete account" });
-            settings.Add(new SettingsItem() { Name = "profile", Description = "name, set an avatar, status" });
-            settings.Add(new SettingsItem() { Name = "chat settings", Description = "auto-download, wallpaper, backup" });
-            settings.Add(new SettingsItem() { Name = "lock screen", Description = "add WAWP on a lock screen" });
-            settings.Add(new SettingsItem() { Name = "notification sounds", Description = "change notification sounds" });
-            settings.Add(new SettingsItem() { Name = "rotation", Description = "auto-rotate or block rotation" });
-            settings.Add(new SettingsItem() { Name = "connectivity", Description = "change connectivity type" });
+            settings.Add(new SettingsItem() { Name = "about", Description = "meet the Team", Destination = "AboutPage"});
+            settings.Add(new SettingsItem() { Name = "contacts", Description = "invite a friend, block", Destination = "ContactsPage" });
+            settings.Add(new SettingsItem() { Name = "account", Description = "privacy, delete account", Destination = "AccountPage" });
+            settings.Add(new SettingsItem() { Name = "profile", Description = "name, set an avatar, status", Destination = "ProfilePage" });
+            settings.Add(new SettingsItem() { Name = "chat settings", Description = "auto-download, wallpaper, backup", Destination = "ChatSettingsPage" });
+            settings.Add(new SettingsItem() { Name = "lock screen", Description = "add WAWP on a lock screen", Destination = "LockScreenPage" });
+            settings.Add(new SettingsItem() { Name = "notification sounds", Description = "change notification sounds", Destination = "NotificationsPage" });
+            settings.Add(new SettingsItem() { Name = "rotation", Description = "auto-rotate or block rotation", Destination = "RotationPage" });
+            settings.Add(new SettingsItem() { Name = "connectivity", Description = "change connectivity type", Destination = "ConnectivityPage" });
 
             SettingsList.ItemsSource = settings;
+
+        }
+
+        private void SettingsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If selected item is null, do nothing
+            if (SettingsList.SelectedItem == null)
+                return;
+
+            // Get the selected item
+            SettingsItem selectedItem = SettingsList.SelectedItem as SettingsItem;
+
+            // Navigate to the next page using the 'dest' property
+            NavigationService.Navigate(new Uri("/Pages/Settings/" + selectedItem.Destination + ".xaml", UriKind.Relative));
+
+            // Reset selected item to null
+            SettingsList.SelectedItem = null;
         }
     }
 }
