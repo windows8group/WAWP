252134dbb87c3280d59e3de28283adde14610ff6
PickaxeCar <82699403+PickaxeCar@users.noreply.github.com>
2024-10-04 18:32:37 +0300

    Add files via upload
    chat settings page has been created. AppResources for english will be imported soon

--- WAWP/Pages/Settings/ChatSettingsPage.xaml.cs
+++ WAWP/Pages/Settings/ChatSettingsPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using WAWP.Resources;
 
 namespace WAWP.Pages.Settings
 {
@@ -15,6 +16,19 @@ namespace WAWP.Pages.Settings
         public ChatSettingsPage()
         {
             InitializeComponent();
+            TogglesList.ItemsSource = new List<SettingsItem>();
+
+            TogglesList.ItemsSource.Add(new SettingsItem() { Name = AppResources.Enter });
+            TogglesList.ItemsSource.Add(new SettingsItem() { Name = AppResources.ShowMessage });
+
+            header.Text = AppResources.Chat_Settings;
+
+            SettingsList.ItemsSource = new List<SettingsItem>();
+
+            SettingsList.ItemsSource.Add(new SettingsItem() { Name = AppResources.Background, Description = AppResources.Background_Des, Destination = "ReplaceMe" });
+            SettingsList.ItemsSource.Add(new SettingsItem() { Name = AppResources.TextSize, Description = AppResources.TextSize_Des, Destination = "ReplaceMe" });
+            SettingsList.ItemsSource.Add(new SettingsItem() { Name = AppResources.MediaAuto, Description = AppResources.MediaAuto_Des, Destination = "ReplaceMe" });
+            SettingsList.ItemsSource.Add(new SettingsItem() { Name = AppResources.Backup, Description = AppResources.Backup_Des, Destination = "ReplaceMe" });
         }
     }
 }
